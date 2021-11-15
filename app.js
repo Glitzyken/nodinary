b819d7fc8321b11cb6d25194c6e7383e6ce828c5
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-11-15 23:05:42 +0100

    🚀 Add uploader - Ability to upload buffered file - Ability to delete file from cloudinary

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log("New package!");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+class Nodinary {
+  constructor(cloudinaryCloudName, cloudinaryApiKey, cloudinaryApiSecret) {
+    this.cloudinaryCloudName = cloudinaryCloudName;
+    this.cloudinaryApiKey = cloudinaryApiKey;
+    this.cloudinaryApiSecret = cloudinaryApiSecret;
+  }
+
+  uploadBuffer = (bufferFile, folderName) => {
+    return new Promise((resolve, reject) => {
+      const cldUploadStream = cloudinary
+        .config({
+          cloud_name: this.cloudinaryCloudName,
+          api_key: this.cloudinaryApiKey,
+          api_secret: this.cloudinaryApiSecret,
+        })
+        .uploader.upload_stream(
+          {
+            folder: `${folderName}`,
+          },
+          (error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          }
+        );
+
+      streamifier.createReadStream(bufferFile).pipe(cldUploadStream);
+    });
+  };
+
+  deleteFile = async (fileId) => {
+    await cloudinary
+      .config({
+        cloud_name: this.cloudinaryCloudName,
+        api_key: this.cloudinaryApiKey,
+        api_secret: this.cloudinaryApiSecret,
+      })
+      .uploader.destroy(fileId);
+  };
+}
+
+module.exports = Nodinary;
